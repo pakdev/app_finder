518d97bad5634c55e7c51fccbac0d09122e7a6ff
Peter Kurlak <pkurlak@gmail.com>
2018-11-04 01:16:56 -0500

    Add comments with helpful links

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::path::Path;
 
 // fn get_applications(dir: &Path) -> HashMap<&str, str> {
 //     let mut applications = HashMap::new();
+// description of *.cpl files: https://support.microsoft.com/en-us/help/149648/description-of-control-panel-cpl-files
+// list of executable file extensions: https://www.lifewire.com/list-of-executable-file-extensions-2626061
 // }
 
 fn main() {
